5bb05cecb89cc3858a22a3798d1bde9826491ed3
YennyVelez <desarrollo@cbit-online.com>
2020-04-28 23:17:45 -0500

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,8 +16,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://emily_2020:5ETB8ND3fZCVSxPV@cluster0-4mcn5.mongodb.net/cafe?retryWrites=true&w=majority';
-    // urlDB = 'mongodb://emily_2020:5ETB8ND3fZCVSxPV@cluster0-4mcn5.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
+
 }
 
 process.env.URLDB = urlDB;
